92b1d7cdbf9ad34187b9faae8845a6c9392daf21
Sanskar-12 <sanskarv2004@gmail.com>
2024-10-13 14:53:37 +0530

    dashboard widget section made

--- admin-dashboard/src/pages/Dashboard.tsx
+++ admin-dashboard/src/pages/Dashboard.tsx
@@ -1,12 +1,114 @@
-import AdminSidebar from "../components/AdminSidebar"
+import { BsSearch } from "react-icons/bs";
+import AdminSidebar from "../components/AdminSidebar";
+import { FaRegBell } from "react-icons/fa";
+import userImg from "../assets/images/userpic.png";
+import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 
 const Dashboard = () => {
   return (
     <div className="admin-container">
-      <AdminSidebar/>
-      <main>main</main>
+      <AdminSidebar />
+      <main className="dashboard">
+        {/* Nav Bar */}
+        <div className="bar">
+          <BsSearch />
+          <input type="text" placeholder="Search for data, users, docs" />
+          <FaRegBell />
+          <img src={userImg} alt="User Image" />
+        </div>
+        {/* Widget section */}
+        <section className="widget-container">
+          <WidgetItem
+            heading="Revenue"
+            percent={40}
+            amount={true}
+            value={340000}
+            color="rgb(0,115,255)"
+          />
+          <WidgetItem
+            percent={-14}
+            value={400}
+            heading="Users"
+            color="rgb(0 198 202)"
+          />
+          <WidgetItem
+            percent={80}
+            value={23000}
+            heading="Transactions"
+            color="rgb(255 196 0)"
+          />
+          <WidgetItem
+            percent={30}
+            value={1000}
+            heading="Products"
+            color="rgb(76 0 255)"
+          />
+        </section>
+        {/* Graph Section */}
+        <section className="graph-container">
+          <div className="revenue-chart">
+            <h2>Revenue & Transaction</h2>
+            {/* Graph Component here */}
+          </div>
+          <div className="dashboard-categories">
+            <h2>Inventory</h2>
+            <div></div>
+          </div>
+        </section>
+      </main>
     </div>
-  )
+  );
+};
+
+interface WidgetItemProps {
+  heading: string;
+  value: number;
+  percent: number;
+  color: string;
+  amount?: boolean;
 }
 
-export default Dashboard
+const WidgetItem = ({
+  heading,
+  value,
+  percent,
+  color,
+  amount,
+}: WidgetItemProps) => {
+  return (
+    <article className="widget">
+      <div className="widget-info">
+        <p>{heading}</p>
+        <h4>{amount ? `$${value}` : value}</h4>
+        {percent > 0 ? (
+          <span className="green">
+            <HiTrendingUp /> +{percent}%
+          </span>
+        ) : (
+          <span className="red">
+            <HiTrendingDown /> {percent}%
+          </span>
+        )}
+      </div>
+      <div
+        className="widget-circle"
+        style={{
+          background: `conic-gradient(
+        ${color} ${(Math.abs(percent) / 100) * 360}deg,
+        rgb(255, 255, 255) 0
+      )`,
+        }}
+      >
+        <span
+          style={{
+            color: color,
+          }}
+        >
+          {percent}%
+        </span>
+      </div>
+    </article>
+  );
+};
+
+export default Dashboard;
