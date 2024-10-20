3aa9d07b2cc3e0a1bdfdf478edbf8e3f650b6443
Sanskar-12 <sanskarv2004@gmail.com>
2024-10-20 13:40:57 +0530

    pie chart done

--- admin-dashboard/src/components/Charts.tsx
+++ admin-dashboard/src/components/Charts.tsx
@@ -148,3 +148,40 @@ export const DoughnutChart = ({
 
   return <Doughnut data={DoughnutData} options={DoughnutOptions} />;
 };
+
+interface PieChartProps {
+  labels: string[];
+  data: number[];
+  backgroundColor: string[];
+  offset?: number[];
+}
+
+export const PieChart = ({
+  labels,
+  data,
+  backgroundColor,
+  offset,
+}: PieChartProps) => {
+  const PieChartData: ChartData<"pie", number[], string> = {
+    labels,
+    datasets: [
+      {
+        data,
+        backgroundColor,
+        borderWidth: 1,
+        offset,
+      },
+    ],
+  };
+
+  const PieChartOptions: ChartOptions<"pie"> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+  return <Pie data={PieChartData} options={PieChartOptions} />;
+};
