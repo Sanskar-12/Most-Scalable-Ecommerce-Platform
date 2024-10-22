3ad504737bed90207146ba03469b25732252b4c0
Sanskar-12 <sanskarv2004@gmail.com>
2024-10-22 10:14:18 +0530

    toss app created

--- admin-dashboard/src/pages/apps/Toss.tsx
+++ admin-dashboard/src/pages/apps/Toss.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import AdminSidebar from "../../components/AdminSidebar";
 
 const Toss = () => {
-  return (
-    <div>Toss</div>
-  )
-}
+  const [angle, setAngle] = useState<number>(0);
 
-export default Toss
+  const flip = () => {
+    if (Math.random() > 0.5) setAngle((prev) => prev + 180);
+    else setAngle((prev) => prev + 360);
+  };
+
+  return (
+    <div className="admin-container">
+      <AdminSidebar />
+      <main className="dashboard-app-container">
+        <h1>Toss</h1>
+        <section>
+          <article
+            className="tosscoin"
+            onClick={flip}
+            style={{
+              transform: `rotateY(${angle}deg)`,
+            }}
+          >
+            <div></div>
+            <div></div>
+          </article>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Toss;
