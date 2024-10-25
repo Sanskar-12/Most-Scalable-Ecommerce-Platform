a4f582bc3ad871dd2570eb7789dfb16f52b377d0
Sanskar-12 <sanskarv2004@gmail.com>
2024-10-25 11:03:50 +0530

    Cart ongoing

--- frontend/src/pages/cart.tsx
+++ frontend/src/pages/cart.tsx
@@ -1,7 +1,38 @@
-const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+import { useState } from "react";
 
-export default Cart
+const cartItems = [];
+const subTotal = 4000;
+const tax = Math.round(subTotal * 0.18);
+const shippingCharges = 200;
+const discount = 200;
+const total = subTotal + tax + shippingCharges;
+
+const Cart = () => {
+  const [coupon, setCoupon] = useState<string>("");
+  const [isValidCoupon, setIsValidCoupon] = useState<boolean>(false);
+
+  return (
+    <div className="cart">
+      <main></main>
+      <aside>
+        <p>Subtotal: ₹{subTotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em> - ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total: ₹{total}</b>
+        </p>
+        <input
+          type="text"
+          placeholder="Coupon Code"
+          value={coupon}
+          onChange={(e) => setCoupon(e.target.value)}
+        />
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
