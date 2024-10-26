058d3e03458d16ecf5e6212b0239c02f6c8e0750
Sanskar-12 <sanskarv2004@gmail.com>
2024-10-26 19:37:28 +0530

    cart ongoing

--- frontend/src/pages/cart.tsx
+++ frontend/src/pages/cart.tsx
@@ -1,6 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItem from "../components/Cart-Item";
 
-const cartItems = [];
+const cartItems = [
+  {
+    productId: "asdfsdf",
+    name: "Macbook Pro",
+    price: 100000,
+    stock: 232,
+    photo: "https://m.media-amazon.com/images/I/71ItMeqpN3L._AC_UY218_.jpg",
+  },
+];
 const subTotal = 4000;
 const tax = Math.round(subTotal * 0.18);
 const shippingCharges = 200;
@@ -11,9 +21,25 @@ const Cart = () => {
   const [coupon, setCoupon] = useState<string>("");
   const [isValidCoupon, setIsValidCoupon] = useState<boolean>(false);
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (Math.random() > 0.5) setIsValidCoupon(true);
+      else setIsValidCoupon(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeoutId);
+      setIsValidCoupon(false);
+    };
+  }, [coupon]);
+
   return (
     <div className="cart">
-      <main></main>
+      <main>
+        {cartItems.map((cartitem, index) => (
+          <CartItem key={index} cartItem={cartitem} />
+        ))}
+      </main>
       <aside>
         <p>Subtotal: ₹{subTotal}</p>
         <p>Shipping Charges: ₹{shippingCharges}</p>
@@ -30,6 +56,16 @@ const Cart = () => {
           value={coupon}
           onChange={(e) => setCoupon(e.target.value)}
         />
+        {coupon &&
+          (isValidCoupon ? (
+            <span className="green">
+              ₹{discount} off using the <code>{coupon}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invlaid Coupon <VscError />
+            </span>
+          ))}
       </aside>
     </div>
   );
