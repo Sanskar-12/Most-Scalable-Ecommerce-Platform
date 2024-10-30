1babd3e69a3602a9c4aed4f4f7530be027215a9e
Sanskar-12 <sanskarv2004@gmail.com>
2024-10-30 13:26:38 +0530

    file upload with multer done

---
+++ backend/src/middlewares/multer.ts
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "uploads");
+  },
+  filename(req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+
+const singleUpload = multer({ storage }).single("file");
+
+export default singleUpload;
