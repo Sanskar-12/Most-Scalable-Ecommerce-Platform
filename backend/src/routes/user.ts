8d1b7fda2812522e21c472e611b72caec76c7c12
Sanskar-12 <sanskarv2004@gmail.com>
2024-10-30 13:01:45 +0530

    admin only middleware made

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,18 +1,28 @@
-import express from "express"
-import { deleteUser, getAllUsers, getUserDetails, newUser } from "../controllers/user.js"
+import express from "express";
+import {
+  deleteUser,
+  getAllUsers,
+  getUserDetails,
+  newUser,
+} from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
 
-const router=express.Router()
+const router = express.Router();
+
+// ----------------All Users Routes----------------//
 
 // POST - /api/v1/user/new
-router.post("/new",newUser)
-
-// GET - /api/v1/user/all
-router.get("/all",getAllUsers)
+router.post("/new", newUser);
 
 // GET - /api/v1/user/<id>
-router.get("/:id",getUserDetails)
+router.get("/:id", getUserDetails);
+
+//---------------- Admin Only Routes--------------//
+
+// GET - /api/v1/user/all
+router.get("/all", adminOnly, getAllUsers);
 
 // DELETE - /api/v1/user/<id>
-router.delete("/:id",deleteUser)
+router.delete("/:id", adminOnly, deleteUser);
 
-export default router
+export default router;
