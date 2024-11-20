0e632d770358cbb97756f676043adfb7b33ed202
Sanskar-12 <sanskarv2004@gmail.com>
2024-11-20 19:29:34 +0530

    added page not found

--- frontend/src/components/Page404.tsx
+++ frontend/src/components/Page404.tsx
@@ -1,5 +1,15 @@
+import { MdError } from "react-icons/md";
+import { Link } from "react-router-dom";
+
 const Page404 = () => {
-  return <div>404</div>;
+  return (
+    <div className="container not-found">
+      <MdError />
+      <h1>Page Not Found</h1>
+      <p>click below to go to home page.</p>
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
 };
 
 export default Page404;
